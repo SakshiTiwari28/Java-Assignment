a7a6280851c12b15cdeeae19646f12daa4efcaa9
SakshiTiwari28 <sakshitiwari776@gmail.com>
2019-07-27 11:58:30 -0700

    MavenEclipse Folder Added

---
+++ MavenEclipse/PhoenixShoppe/src/main/java/com/phoenix/shop/entities/Product.java
@@ -0,0 +1,49 @@
+package com.phoenix.shop.entities;
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Table
+public class Product {
+	@Id
+    private int productId;
+    private String name;
+    private String brand;
+    private float price;
+    
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getBrand() {
+		return brand;
+	}
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	public Product(int productId, String name, String brand, float price) {
+		super();
+		this.productId = productId;
+		this.name = name;
+		this.brand = brand;
+		this.price = price;
+	}
+	public Product() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+}
