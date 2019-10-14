10cb08ff8ec9b104c4d86bfb16cb3f90f6e9c45e
SakshiTiwari28 <37017762+SakshiTiwari28@users.noreply.github.com>
2019-10-13 23:39:29 -0700

    Add files via upload

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,42 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { CustomerComponent } from './customer/customer.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-import { OrderComponent } from './order/order.component';
-import { CategoryComponent } from './category/category.component';
-import { ProductMenuComponent } from './product-menu/product-menu.component';
-import { CustomerMenuComponent } from './customer-menu/customer-menu.component';
-
+import { EmployeeComponent } from './employee/employee.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
 
 
 const routes: Routes = [
-
-  {
-    path : 'login',
-    component: LoginComponent
-  },
-  {
-    path : 'product-menu',
-    component : ProductMenuComponent
-  },
-  {
-    path: 'customer',
-    component : CustomerComponent
-  },
-  {
-    path: 'customer-menu',
-    component : CustomerMenuComponent
-  },
-  {
-    path:'order',
-    component :OrderComponent
-  },
-  {
-    path:'category',
-    component:CategoryComponent
-  }
+  { path : '' , component : EmployeeComponent },
+  { path : 'addemployee', component : AddEmployeeComponent}
 ];
 
 @NgModule({
@@ -44,5 +14,3 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-
-
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,55 +3,27 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GreetingComponent } from './greeting/greeting.component';
-import { LoginComponent } from './login/login.component';
-import { FooterComponent } from './footer/footer.component';
-import { FormsModule } from '@angular/forms';
-import { ProductComponent } from './product/product.component';
-import { CustomerComponent } from './customer/customer.component';
-import { ProductListComponent } from './product-list/product-list.component';
-import { CustomerListComponent } from './customer-list/customer-list.component';
-import { CustomerDetailComponent } from './customer-detail/customer-detail.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { OrderComponent } from './order/order.component';
-import { CategoryComponent } from './category/category.component';
-import { ProductMenuComponent } from './product-menu/product-menu.component'
-import { ProductModule } from './product/product.module';
-import { CustomerMenuComponent } from './customer-menu/customer-menu.component';
-import { HighlightDirective } from './highlight.directive';
+import { EmployeeComponent } from './employee/employee.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { HeaderComponent } from './header/header.component';
+import { HttpClient } from 'selenium-webdriver/http';
 import { HttpClientModule } from '@angular/common/http';
-import { ProductService } from './product.service';
-import { DeleteComponent } from './delete/delete.component';
-
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    GreetingComponent,
-    LoginComponent,
-    FooterComponent,
-    ProductComponent,
-    CustomerComponent,
-    ProductListComponent,
-    CustomerListComponent,
-    CustomerDetailComponent,
-    ProductDetailComponent,
-    OrderComponent,
-    CategoryComponent,
-    ProductMenuComponent,
-    CustomerMenuComponent,
-    HighlightDirective,
-    DeleteComponent
+    EmployeeComponent,
+    AddEmployeeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
-    ProductModule,
-    BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [ProductService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
